6c8ed3d7c0723fc6db197f64f4bfd79529a99277
Aysan Isayo <aisayo@icloud.com>
2022-06-30 08:12:46 -0400

    lecture 4

--- 04_sql_and_databases/lib/savable.rb
+++ 04_sql_and_databases/lib/savable.rb
@@ -1,14 +1,9 @@
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+
 class Savable
 
   @@all = {}
-
-  def self.new_from_row(row)
-    row.transform_keys{|k| k.to_sym} # will convert string keys to symbols
-  end
-
-  def self.table_name
-    self.name.tableize
-  end
 
   def self.all
     @@all[self] ||= DOGS_DB.execute("SELECT * FROM #{self.table_name}").map do |row|
@@ -16,18 +11,65 @@ class Savable
     end
   end
 
-  def self.create(attributes = {})
-    self.new(attributes).save
+  def self.new_from_row(row)
+    self.new(row.transform_keys(&:to_sym))
   end
 
-  def initialize(attributes = {})
-    attributes.each do |attribute, value|
-      self.send("#{attribute}=", value)
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
+
+  def self.column_names
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DOGS_DB.execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      if self.respond_to?("#{property.to_s}=") 
+        self.send("#{property.to_s}=", value) 
+    end 
     end
   end
 
   def save
-    self.class.all << self
-    self
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DOGS_DB.execute(sql)
+    @id = DOGS_DB.execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DOGS_DB.execute(sql)
+  end
+
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DOGS_DB.execute(sql)
   end
 end
